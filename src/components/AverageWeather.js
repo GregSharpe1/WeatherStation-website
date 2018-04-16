7296530f718d346301974ecdeb790d909c02853c
Greg Sharpe <me@gregsharpe.co.uk>
2018-04-16 21:11:09 +0100

    Implemented an average component, through the use of Date Picker

---
+++ src/components/AverageWeather.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import moment from 'moment';
+import '../assets/currentweather.css'
+
+
+class AverageWeather extends Component {
+    constructor(props) {
+        super(props);
+        this.processAverages = this.processAverages.bind(this);
+    
+        this.state = { 
+          last_data_received: [],
+          averages: {}
+        }
+    }
+  
+  	processAverages(dataset){
+        var count = dataset.length
+        var keys = Object.keys(dataset[0].payload)
+        var averages = {}
+        
+        for(var x=0; x<dataset.length; x++) {
+        for(var y=0; y<keys.length; y++){
+            if(keys[y] !== 'wind_direction'){
+                if(averages[keys[y]]) {
+                    averages[keys[y]] = averages[keys[y]] + parseFloat(dataset[x].payload[keys[y]])
+                } else {
+                    averages[keys[y]] = parseFloat(dataset[x].payload[keys[y]])
+                }
+            }
+          }
+        }
+      
+      	for(var i=0; i<keys.length; i++){
+          averages[keys[i]] = Math.round((averages[keys[i]] / count) * 100) / 100
+        }
+      
+      return averages
+    }
+  
+    componentWillReceiveProps(nextProps) {
+        if(JSON.stringify(this.props.last_data_received) !== JSON.stringify(nextProps.data)) {
+          if(nextProps.data.length > 0){
+            this.setState({
+                last_data_received: nextProps.data,
+                averages: this.processAverages(nextProps.data)
+            })
+          } else {
+            this.setState({
+              last_data_received: [],
+              averages: {}
+            })
+          }
+        }
+    } 
+
+    render() {
+        if (this.state.averages.temperature_indoor) {
+        return (
+        <table>
+            <tbody>
+                <tr key = '1' >
+                  <div class="container">
+                    <div class="row">
+                      <div class="weather_widgets">
+                        <ul>
+                          <li>
+                            <div class="widget_container">
+                              <h1>{this.state.averages.temperature_indoor}<small>&#x2103;</small></h1>
+                              <h3>Temperature Indoor</h3>
+                            </div>
+                          </li>
+                          <li>
+                            <div class="widget_container">
+                              <h1>{this.state.averages.humidity_outdoor}<small>%</small></h1>
+                              <h3>Humidity Outdoor</h3>
+                            </div>
+                          </li>
+                          <li>
+                            <div class="widget_container">
+                              <h1>{this.state.averages.temperature_outdoor}<small>&#x2103;</small></h1>
+                              <h3>Temperature Outdoor</h3>
+                            </div>
+                          </li>
+                          <li>
+                            <div class="widget_container">
+                              <h1>{this.state.averages.humidity_indoor}<small>%</small></h1>
+                              <h3>Humidity Indoor</h3>
+                            </div>
+                          </li>
+                          <li>
+                            <div class="widget_container">
+                              <h1>{this.state.averages.air_pressure}<small>hPa</small></h1>
+                              <h3>Air Pressure</h3>
+                            </div>
+                          </li>
+                          <li>
+                            <div class="widget_container">
+                              <h1>{this.state.averages.rain_fall}<small>mm</small></h1>
+                            	<h3>Rainfall</h3>
+                            </div>
+                          </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </div>
+                </tr>
+           </tbody>
+        </table>
+        )
+    } else
+    { return (
+        <div>
+            <p>
+               Oops, something went wrong at this date, no readings were taken. Try another date
+            </p>
+        </div>
+    )}
+}}
+
+export default AverageWeather;  
--- src/components/PreviousWeather.js
+++ src/components/PreviousWeather.js
@@ -5,24 +5,79 @@ import Chart from './Charts';
 import moment from 'moment';
 import '../assets/previousweather.css'
 
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+import AverageWeather from './AverageWeather';
+
+
+// PreviousWeather
+// Child -> DatePicker
+// Return the 'DatePicker's startDate state'
 class PreviousWeather extends Component {
     constructor(props) {
         super(props);
+      	var now = moment()
         this.state = {
-        }
+          data: [],
+          selected_date: now,
+          start_timestamp: now.startOf('day').valueOf(),
+          end_timestamp: now.endOf('day').valueOf()
+        };
+      	this.handleSelectDate = this.handleSelectDate.bind(this);
+    }
+  
+  	handleSelectDate(date) {
+      //valueOf provides the AWS compliant timestamp.
+      //.startOf allows us to find the start of the date selected.
+      var start_timestamp = date.startOf('day').valueOf()
+      //.endOf allows us to find the start of the date selected.
+      var end_timestamp = date.endOf('day').valueOf()
+      
+      this.setState({
+        selected_date: date,
+        start_timestamp: start_timestamp,
+        end_timestamp: end_timestamp
+      })
+      
+			//Prepare API Call
+      var location = "aber" || this.state.location;
+      var base_url  = "https://api.gregsharpe.co.uk/weather/history/" + location + "/" + start_timestamp + "/" + end_timestamp;
+      console.log('Base URL: ', base_url);
 
+      axios.get(base_url)
+      .then(res => {
+        this.setState({
+          data: res.data.data.weather_data,
+        })
+      })
+      // error handling
+      .catch(err => {
+        console.log(err);
+      });
     }
 
     render() {
-      console.log(this.state.value)
+      console.log("STATE: ", this.state)
+      if (this.state.data) {
         return (
-        <div>
-          <center>
-            Please enter a date, you wish to see the Weather Readings of.
+          <div>
+            <center>
+              Please enter a date, you wish to see the Weather Readings of.
+              <DatePicker
+                onSelect={this.state.start_date}
+                onChange={this.handleSelectDate}
+              />
+            <h1>Previous Weather for: {moment(this.state.start_timestamp).format("DD MMM YYYY")}</h1>
+            <div>
+              <h2>Average Weather</h2>
+              <AverageWeather data={this.state.data} />
+            </div>
           </center>
         </div>
         )
-    }
+    } 
+  }
 }
 
 export default PreviousWeather;
